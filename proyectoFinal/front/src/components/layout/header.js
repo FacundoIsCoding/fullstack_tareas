39e6c4fb6761de6588f128d69ce5d17b4b7284f7
Facundo Lopez <lopezfacundo61ar@gmail.com>
2023-06-22 12:10:10 -0300

    react I

---
+++ proyectoFinal/front/src/components/layout/header.js
@@ -0,0 +1,14 @@
+const Header = (props) => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Nosotros</li>
+                <li>Novedades</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header;
